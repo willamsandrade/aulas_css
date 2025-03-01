e356dcb1fefba62f8038e1eca40e4c0d7fe2f11c
Willams Silva Andrade <85310845+willamsandrade@users.noreply.github.com>
2025-03-01 09:26:52 -0300

    ajustes

---
+++ GYM Website Source Code/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-links a");
+
+    function changeActiveLink() {
+        let currentSection = "home"; // Começa com Home ativo
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop - 100;
+            if (window.scrollY >= sectionTop) {
+                currentSection = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").includes(currentSection)) {
+                link.classList.add("active");
+            }
+        });
+    }
+
+    // Chamar função ao carregar para manter o "Home" ativo no início
+    changeActiveLink();
+    window.addEventListener("scroll", changeActiveLink);
+});
